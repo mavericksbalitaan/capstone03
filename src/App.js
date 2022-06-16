c5b42b8f40051728bde5e7c54b83c566fbe66429
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-16 23:21:24 +0500

    Create home and header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
