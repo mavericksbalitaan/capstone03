66a42ffb93666aa3efb8293b76663e9417a89911
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-21 05:12:42 +0500

    Add test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './components/redux/configureStore';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const MockApp = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+describe('Check if App Component renders correctly', () => {
+  test('renders heading', () => {
+    render(<MockApp />);
+    const headingElement = screen.getByRole('heading', {
+      name: /who's that pokemon\?/i,
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('renders textbox', () => {
+    render(<MockApp />);
+    const textBoxElement = screen.getByRole('textbox');
+    expect(textBoxElement).toBeInTheDocument();
+  });
+
+  test('renders button', () => {
+    render(<MockApp />);
+    const buttonElement = screen.getByRole('button', {
+      name: /check answer/i
+    });
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test('renders heading', () => {
+    render(<MockApp />);
+    const headingElement = screen.getByRole('heading', {
+      name: /pokemon by id/i
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
 });
